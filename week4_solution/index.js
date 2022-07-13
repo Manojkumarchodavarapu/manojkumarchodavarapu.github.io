ac5a39ca327955b8eb9122d8e3f8167192dde702
Manojkumarchodavarapu <94033770+Manojkumarchodavarapu@users.noreply.github.com>
2022-07-13 12:32:20 +0530

    Add files via upload

---
+++ week4_solution/index.js
@@ -0,0 +1,14 @@
+var names = ["Yaameen","john","jen","jason","paul","Frank","Larry","paula","Laura","Jim"];
+console.log("Hello "+ names[0]);
+for(var i=1; i<4; i++)
+{
+    console.log("GoodBye "+ names[i]);
+}
+for(var i=4; i<9; i++)
+{
+    console.log("Hello "+ names[i]);
+}
+for(var i=9; i<10; i++)
+{
+    console.log("GoodBye "+ names[i]);
+}
